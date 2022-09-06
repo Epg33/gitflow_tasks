b55fd96af038743970a677b6b87a6864e9c08de9
samuelrm5 <srodriguez438@misena.edu.co>
2022-09-06 17:50:25 -0500

    maquetacion

---
+++ client/src/components/Registro.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Account() {
+    return (
+        <>
+            <form action="">
+                <input placeholder='Ingrese Numero De Documento' type="text" />
+                <input placeholder='Ingrese Nombres' type="text" />
+                <input placeholder='Ingrese Apellidos' type="text" />
+                <input placeholder='Ingre Edad' type="number" />
+                <input placeholder='Ingrese Contraseña' type="text" />
+                <input placeholder="Verificar Contraseña" type="text" />
+            </form>
+        </>
+    )
+}
+
+export default Account
---
+++ client/src/components/login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Account() {
+    return (
+        <>
+            <form action="">
+                <h3>Login</h3>
+                <input placeholder='Ingresar documento' />
+                <input placeholder='Ingrese contraseña' type="text" />
+                <button>Ingresar</button>
+            </form>
+        </>
+    )
+}
+
+export default Account
